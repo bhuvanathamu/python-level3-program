bc96be2cdb20e084af4f1a6bb8c9822323e7c9cc
bhuvanathamu <bhuvanakutty02@gmail.com>
2025-08-18 10:07:50 +0530

    program14

---
+++ program14.py
@@ -0,0 +1,23 @@
+print()
+for i in range(0,1,9):
+    print(i,end=",")
+print()
+for i in range(1,7):
+    print(i,end=",")
+print()
+for i in range(7,9):
+    print(i,end=",")
+print()
+for i in range(9,11):
+    print(i,end=",")
+print()
+for i in range(4,10):
+    print(i,end=",")
+print()
+for i in range(8,9,20):
+    print(i,end=",")
+print()
+for i in range(10,20):
+    print(i,end=",")
+      
+      
