bd51a9cbee6b8ea004092e5573c71b62ddefaaad
bhuvanathamu <bhuvanakutty02@gmail.com>
2025-08-18 10:09:22 +0530

    program15

---
+++ program15.py
@@ -0,0 +1,7 @@
+print(" Table program")
+print("---------------")
+n=int(input("enter the number:"))
+print("Table")
+print("---------------")
+for i in range(1,11):
+    print(str(i)+"x"+str(n)+"="+str(i*n))
